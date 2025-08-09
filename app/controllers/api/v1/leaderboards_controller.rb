10cd034cab53c64fc4d3e3926974e2effe4b6ed1
evabanegacom <udegbue69@gmail.com>
2025-08-09 14:27:15 +0100

    testing api

--- app/controllers/api/v1/leaderboards_controller.rb
+++ app/controllers/api/v1/leaderboards_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::LeaderboardsController < ApplicationController
-  # before_action :authenticate_user
+  before_action :authenticate_user
 
   def index
     period = params[:period].in?(%w[weekly monthly all_time]) ? params[:period] : 'all_time'
